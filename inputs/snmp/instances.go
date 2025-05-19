abd65a3b276fb503d40b1dcca14c9b8859a804ec
German Generalov <g.generalov@gagarin.me>
2025-05-19 20:28:15 +0300

    fix snmp

--- inputs/snmp/instances.go
+++ inputs/snmp/instances.go
@@ -376,12 +376,10 @@ func fastPingRtt(ip string, timeout int) (float64, error) {
 	var rt float64
 	rt = -1
 	p := fastping.NewPinger()
-	p.Debug = true
 	ra, err := net.ResolveIPAddr("ip4:icmp", ip)
 	if err != nil {
 		return -1, err
 	}
-	fmt.Println(123123)
 	p.AddIPAddr(ra)
 	p.OnRecv = func(addr *net.IPAddr, rtt time.Duration) {
 		rt = float64(rtt.Microseconds())
